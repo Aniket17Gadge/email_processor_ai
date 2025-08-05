2a27a17e6ccacd559e133eeb435bb95ff6eb1f02
Aniket17Gadge <aniketgadge17@gmail.com>
2025-08-06 00:11:30 +0530

    email analyzer agent setup complete

--- Email_processor_AI/email_processor_ai/email_processor_ai/settings.py
+++ Email_processor_AI/email_processor_ai/email_processor_ai/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-v8a*@j0heh@h-rn2-m9&mmoqmq82off@+_0=h^na(-=xg1$e#z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'email_processor_app',
+    'rest_framework', 
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Add this line
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React local dev server
 ]
 
 ROOT_URLCONF = 'email_processor_ai.urls'
